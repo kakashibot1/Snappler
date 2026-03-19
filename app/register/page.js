2eb0b5f1a4a943ed089a38777030394ba3f22ed0
octavio wina <botkakashi234@gmail.com>
2026-03-19 22:55:11 +0100

    Create page.js

---
+++ app/register/page.js
@@ -0,0 +1,13 @@
+export default function Register() {
+  return (
+    <div className="container">
+      <div className="card">
+        <h2>Inscription</h2>
+        <input type="text" placeholder="Nom" />
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Mot de passe" />
+        <button>S'inscrire</button>
+      </div>
+    </div>
+  );
+    }

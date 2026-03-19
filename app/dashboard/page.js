44607c55d1a42aca9e7e0bb8906234fa54c3fd59
octavio wina <botkakashi234@gmail.com>
2026-03-19 23:02:46 +0100

    Create page.js

---
+++ app/dashboard/page.js
@@ -0,0 +1,10 @@
+export default function Dashboard() {
+  return (
+    <div className="container">
+      <h2>Dashboard</h2>
+      <div className="card">
+        <p>Bienvenue 👋</p>
+      </div>
+    </div>
+  );
+    }

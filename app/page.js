d0a4fae67d9da60ae1935eb2cb5197d2b5059c2a
octavio wina <botkakashi234@gmail.com>
2026-03-19 22:50:51 +0100

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="container">
+      <h1>Snappler 🚀</h1>
+
+      <div className="card">
+        <p>Ton réseau social moderne 🌐</p>
+      </div>
+    </div>
+  );
+}
